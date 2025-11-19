fd3c9f47e0d05b2049420f911975ef93909ffdc9
ALi <alieissa789@gmail.com>
2025-11-19 16:52:38 +0200

    added the db_helper for editing the db

---
+++ app/db/db_helper.py
@@ -0,0 +1,29 @@
+import sqlite3
+from .init_db import DB_PATH
+def get_connection():
+    conn=sqlite3.connect(DB_PATH)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    return conn
+def execute_query(query, params=()):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+    conn.close()
+def fetch_one(query, params=()):
+ 
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    row = cursor.fetchone()
+    conn.close()
+    return row
+def fetch_all(query, params=()):
+ 
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    rows = cursor.fetchone()
+    conn.close()
+    return rows
