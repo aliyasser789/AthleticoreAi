cee1045535cb7bc623dbee4c2e45a3b434e06d4c
ALi <alieissa789@gmail.com>
2025-11-20 08:03:57 +0200

    flask file done with api for login and registeration

--- run.py
+++ run.py
@@ -1,8 +1,71 @@
+from flask import Flask, request, jsonify
+from app.services.auth_manager import Authentication_manager
 
-from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 @app.route("/")
 def home():
     return "athleticore running"
-if __name__=="__main__":
+
+
+@app.route("/api/auth/register", methods=["POST"])
+def register():
+    data = request.get_json() or {}
+
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+   
+    if not username or not email or not password:
+        return jsonify({"error": "username, email and password are required"}), 400
+
+    
+    user = Authentication_manager.register_user(username, email, password)
+
+
+    if user is None:
+        return jsonify({"error": "Username or email already taken"}), 409
+
+   
+    return jsonify({
+        "message": "User registered successfully",
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        }
+    }), 201
+@app.route("/api/auth/login", methods=["POST"])
+def login():
+    data = request.get_json() or {}
+
+   
+    identifier = data.get("identifier")
+    password = data.get("password")
+
+    
+    if not identifier or not password:
+        return jsonify({"error": "identifier and password are required"}), 400
+
+  
+    user = Authentication_manager.login_user(identifier,password)
+
+    if user is None:
+       
+        return jsonify({"error": "Invalid username/email or password"}), 401
+
+   
+    return jsonify({
+        "message": "Login successful",
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        }
+    }), 200
+
+
+if __name__ == "__main__":
     app.run(debug=True)
